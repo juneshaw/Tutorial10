55665b11b347c1ea035ea8ee53ee2a20b35c070d
June Shaw <june.shaw@me.com>
2016-05-16 20:17:58 -0600

    initial commit

---
+++ src/App.java
@@ -0,0 +1,22 @@
+// Arrays
+public class App {
+
+	public static void main(String[] args) {
+		
+		int number = 7;
+		System.out.println(number);
+		int[] arrayInt = {1,2,3};
+		arrayInt = new int[3];
+		System.out.println(arrayInt[0]);
+		System.out.println(arrayInt[1]);		
+		System.out.println(arrayInt[2]);
+		
+		for (int i = 0; i < arrayInt.length; i++) {
+			System.out.println(arrayInt[i]);
+		}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
